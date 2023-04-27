8a7b256c30155e78677525fbc536d18e83a74843
waveofmymind <sjun0913@gmail.com>
2023-04-27 15:51:06 +0900

    [FIX] 메서드별 커넥션 외부에서 인자로 받도록 수정

--- src/main/java/org/example/simpledb/Sql.java
+++ src/main/java/org/example/simpledb/Sql.java
@@ -16,7 +16,7 @@ public class Sql {
     private final List<Object> parameters;
     private final SimpleDb simpleDb;
 
-    public Sql(SimpleDb simpleDb) {
+    public Sql(SimpleDb simpleDb) throws SQLException {
         this.sqlBuilder = new StringBuilder();
         this.parameters = new ArrayList<>();
         this.simpleDb = simpleDb;
@@ -54,14 +54,20 @@ public class Sql {
         return parameters.toArray();
     }
 
-    public long insert() {
-        Connection conn = null;
+    private PreparedStatement prepareStatement(Connection conn, String sql, Object... parameters) throws SQLException {
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        for (int i = 0; i < parameters.length; i++) {
+            pstmt.setObject(i + 1, parameters[i]);
+        }
+        return pstmt;
+    }
+
+    public long insert(Connection conn) {
         PreparedStatement pstmt = null;
         try {
-            conn = getSimpleDb().getConnection();
             pstmt = conn.prepareStatement(getSql(), Statement.RETURN_GENERATED_KEYS);
 
-            if (getSimpleDb().isDevMode()) {
+            if (simpleDb.isDevMode()) {
                 System.out.println(getSql());
             }
 
@@ -87,21 +93,46 @@ public class Sql {
                     e.printStackTrace();
                 }
             }
-            if (conn != null) {
-                getSimpleDb().closeConnection();
-            }
         }
         return -1;
     }
 
+    public Map<String, Object> selectRow(Connection conn) {
+        Map<String, Object> map = new HashMap<>();
+        try (PreparedStatement pstmt = prepareStatement(conn, getSql(), getParameters());
+             ResultSet rs = pstmt.executeQuery()) {
 
+            if (simpleDb.isDevMode()) {
+                System.out.println(getSql());
+            }
 
-    public long update() {
-        try (Connection conn = DriverManager.getConnection(simpleDb.getUrl(), simpleDb.getUsername(), simpleDb.getPassword());
-             PreparedStatement pstmt = conn.prepareStatement(getSql())) {
+            ResultSetMetaData metaData = rs.getMetaData();
+            int columnCount = metaData.getColumnCount();
+
+            if (rs.next()) { //레코드가 있을 경우
+                for (int i = 1; i <= columnCount; i++) {
+                    //컬럼의 수만큼 모든 데이터를 Map에 넣는다.
+                    String columnName = metaData.getColumnName(i);
+                    Object columnValue = rs.getObject(i);
+                    map.put(columnName, columnValue);
+                }
+                return map;
+            } else {
+                return null;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public long update(Connection conn) {
+        PreparedStatement pstmt = null;
+        try {
+            pstmt = conn.prepareStatement(getSql());
 
             Object[] parameters = getParameters();
-
             for (int i = 0; i < parameters.length; i++) {
                 pstmt.setObject(i + 1, parameters[i]);
             }
@@ -116,16 +147,25 @@ public class Sql {
             if (simpleDb.isDevMode()) {
                 e.printStackTrace();
             }
+        } finally {
+            if (pstmt != null) {
+                try {
+                    pstmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
         return -1;
     }
 
-    public long delete() {
-        try (Connection conn = DriverManager.getConnection(simpleDb.getUrl(), simpleDb.getUsername(), simpleDb.getPassword());
-             PreparedStatement pstmt = conn.prepareStatement(getSql())) {
+
+    public long delete(Connection conn) {
+        PreparedStatement pstmt = null;
+        try {
+            pstmt = conn.prepareStatement(getSql());
 
             Object[] parameters = getParameters();
-
             for (int i = 0; i < parameters.length; i++) {
                 pstmt.setObject(i + 1, parameters[i]);
             }
@@ -135,16 +175,25 @@ public class Sql {
             }
 
             return pstmt.executeUpdate();
+
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            if (pstmt != null) {
+                try {
+                    pstmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
         return -1;
     }
 
-    public LocalDateTime selectDatetime() {
+
+    public LocalDateTime selectDatetime(Connection conn) {
         LocalDateTime datetime = null;
-        try (Connection conn = DriverManager.getConnection(simpleDb.getUrl(), simpleDb.getUsername(), simpleDb.getPassword());
-             Statement stmt = conn.createStatement();
+        try (Statement stmt = conn.createStatement();
              ResultSet rs = stmt.executeQuery(getSql())) {
 
             if (rs.next()) {
@@ -156,161 +205,88 @@ public class Sql {
                 System.out.println(getSql());
             }
 
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return datetime;
     }
 
-    public Long selectLong() {
-        try (Connection conn = DriverManager.getConnection(simpleDb.getUrl(), simpleDb.getUsername(), simpleDb.getPassword());
-             PreparedStatement pstmt = conn.prepareStatement(getSql())) {
-
-            Object[] parameters = getParameters();
-
-            for (int i = 0; i < parameters.length; i++) {
-                pstmt.setObject(i + 1, parameters[i]);
-            }
+    public Long selectLong(Connection conn) {
+        try (PreparedStatement pstmt = prepareStatement(conn, getSql(), getParameters());
+             ResultSet rs = pstmt.executeQuery()) {
 
             if (simpleDb.isDevMode()) {
                 System.out.println(getSql());
             }
 
-            try (ResultSet rs = pstmt.executeQuery()) {
-                return rs.next() ? rs.getLong(1) : null;
-            }
+            return rs.next() ? rs.getLong(1) : null;
+
         } catch (SQLException e) {
             e.printStackTrace();
-
         }
         return null;
     }
 
-    public String selectString() {
-        try (Connection conn = DriverManager.getConnection(simpleDb.getUrl(), simpleDb.getUsername(), simpleDb.getPassword());
-             PreparedStatement pstmt = conn.prepareStatement(getSql())) {
-
-            Object[] parameters = getParameters();
-
-            for (int i = 0; i < parameters.length; i++) {
-                pstmt.setObject(i + 1, parameters[i]);
-            }
+    public String selectString(Connection conn) {
+        try (PreparedStatement pstmt = prepareStatement(conn, getSql(), getParameters());
+             ResultSet rs = pstmt.executeQuery()) {
 
             if (simpleDb.isDevMode()) {
                 System.out.println(getSql());
             }
 
-            try (ResultSet rs = pstmt.executeQuery()) {
-                return rs.next() ? rs.getString(1) : null;
-            }
+            return rs.next() ? rs.getString(1) : null;
 
         } catch (SQLException e) {
             e.printStackTrace();
-
         }
         return null;
     }
 
-    public Map<String, Object> selectRow() {
-        Map<String, Object> map = new HashMap<>();
-        try (Connection conn = DriverManager.getConnection(simpleDb.getUrl(), simpleDb.getUsername(), simpleDb.getPassword());
-             PreparedStatement pstmt = conn.prepareStatement(getSql())) {
-
-            if (simpleDb.isDevMode()) {
-                System.out.println(getSql());
-            }
-
-            try (ResultSet rs = pstmt.executeQuery()) {
-                ResultSetMetaData metaData = rs.getMetaData();
-                int columnCount = metaData.getColumnCount();
-
-                if (rs.next()) { //레코드가 있을 경우
-                    for (int i = 1; i <= columnCount; i++) {
-                        //컬럼의 수만큼 모든 데이터를 Map에 넣는다.
-                        String columnName = metaData.getColumnName(i);
-                        Object columnValue = rs.getObject(i);
-                        map.put(columnName, columnValue);
-                    }
-                    return map;
-                } else {
-                    return null;
-                }
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-
-        }
-        return null;
-    }
-
-    public <T> List<T> selectRows(Class<T> clazz) {
+    public <T> List<T> selectRows(Connection conn, Class<T> clazz) {
         List<T> result = new ArrayList<>();
-        try (Connection conn = DriverManager.getConnection(simpleDb.getUrl(), simpleDb.getUsername(), simpleDb.getPassword());
-             PreparedStatement pstmt = conn.prepareStatement(getSql())) {
+        try (PreparedStatement pstmt = prepareStatement(conn, getSql(), getParameters());
+             ResultSet rs = pstmt.executeQuery()) {
 
             if (simpleDb.isDevMode()) {
                 System.out.println(getSql());
             }
 
-            Object[] parameters = getParameters();
+            ResultSetMetaData metaData = rs.getMetaData();
+            int columnCount = metaData.getColumnCount();
 
-            for (int i = 0; i< parameters.length; i++) {
-                pstmt.setObject(i+1, parameters[i]);
-            }
+            while (rs.next()) {
+                T instance = clazz.getDeclaredConstructor().newInstance();
+                for (int i = 1; i <= columnCount; i++) {
+                    String columnName = metaData.getColumnName(i);
+                    Object columnValue = rs.getObject(i);
 
-            try (ResultSet rs = pstmt.executeQuery()) {
-                ResultSetMetaData metaData = rs.getMetaData();
-                int columnCount = metaData.getColumnCount();
-
-                if(rs.next()) {
-                    T instance = clazz.getDeclaredConstructor().newInstance();
-                    for (int i = 1; i<= columnCount; i++) {
-                        String columnName = metaData.getColumnName(i);
-                        Object columnValue = rs.getObject(i);
-
-                        Field field = clazz.getDeclaredField(columnName);
-                        field.setAccessible(true);
-                        field.set(instance, columnValue);
-                    }
-                    result.add(instance);
+                    Field field = clazz.getDeclaredField(columnName);
+                    field.setAccessible(true);
+                    field.set(instance, columnValue);
                 }
+                result.add(instance);
             }
-
-
         } catch (SQLException | InstantiationException | IllegalAccessException | NoSuchMethodException |
                  InvocationTargetException | NoSuchFieldException e) {
             e.printStackTrace();
         }
-
         return result;
-
-
     }
 
-    public List<Long> selectLongs() {
 
+    public List<Long> selectLongs(Connection conn) {
         List<Long> result = new ArrayList<>();
-        try (Connection conn = DriverManager.getConnection(simpleDb.getUrl(), simpleDb.getUsername(), simpleDb.getPassword());
-             PreparedStatement pstmt = conn.prepareStatement(getSql())) {
+        try (PreparedStatement pstmt = prepareStatement(conn, getSql(), getParameters());
+             ResultSet rs = pstmt.executeQuery()) {
 
             if (simpleDb.isDevMode()) {
                 System.out.println(getSql());
             }
 
-            Object[] parameters = getParameters();
-
-            for (int i = 0; i< parameters.length; i++) {
-                pstmt.setObject(i+1, parameters[i]);
+            while (rs.next()) {
+                result.add(rs.getLong(1));
             }
-
-            try (ResultSet rs = pstmt.executeQuery()) {
-                while (rs.next()) {
-                    result.add(rs.getLong(1));
-                }
-            }
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
